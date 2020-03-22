03c56607544ab9fce5c7e291035eb45ba0029dc3
Torstein Solberg <torstsol@stud.ntnu.no>
2020-03-22 18:51:19 +0100

    added all calculations for part A (a)

--- script.py
+++ script.py
@@ -1,1 +1,86 @@
-print("hello")
+import numpy as np
+
+np.set_printoptions(precision=3)
+
+# The H-h vector
+f = np.asmatrix([[39.632,
+                 39.572,
+                 39.565,
+                 39.520,
+                 39.434,
+                 39.606,
+                 39.665,
+                 39.545,
+                 39.585, 
+                 39.545,
+                 39.535,
+                 39.553,
+                 40.055,
+                 40.120, 
+                 39.778,
+                 39.870,
+                 39.318,
+                 39.337,
+                 39.548,
+                 39.148,
+                 39.555]])
+
+f = f.transpose()
+
+# The weight matrix
+p = np.asmatrix([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4]])
+
+# The A matrix
+A = np.asmatrix(np.zeros((21, 6)))
+input_data = open("input.txt","r")
+
+for i in range(0, 21):
+    X = float(input_data.readline())
+    Y = float(input_data.readline())
+    A[i] = [X*X, X*Y, Y*Y, X, Y, 1]
+    garbage = float(input_data.readline())
+    garbage = float(input_data.readline())
+    garbage = float(input_data.readline())
+    garbage = float(input_data.readline())
+
+# the vector of estimated coefficients
+x = (np.linalg.inv(A.transpose()*p*A))*A.transpose()*p*f
+
+# Estimated corrections
+v = (A*x-f)
+
+n = 21 # Observations
+e = 6 # unknowns
+
+# Standard deviation
+sigma0 = np.sqrt((v.transpose()*p*v)/(n-e))
+
+# Co-factor matrix
+Q = np.linalg.inv(A.transpose()*p*A)
+
+# Variance-covariance matrix
+C = np.multiply(sigma0*sigma0, Q)
+
+print(C)
+
+
